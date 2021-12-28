7c7fbea81a975c747fbe1bc177a77956b7f53a1f
milon2000 <milakozlowska2000@gmail.com>
2021-12-28 14:52:05 +0100

    test(favoriteBlog): define a function that finds out which blog has most likes (4.5)

--- part4/tests/favoriteBlog.test.js
+++ part4/tests/favoriteBlog.test.js
@@ -1,6 +1,6 @@
 const listHelper = require('../utils/listHelper')
 
-describe('total likes', () => {
+describe('favorite blog', () => {
     const blogs = [{
             _id: "5a422a851b54a676234d17f7",
             title: "React patterns",
@@ -50,18 +50,15 @@ describe('total likes', () => {
             __v: 0
         }
     ]
-
-    const favorite = {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-    }
-
-    test('returns the total sum of likes in all of the blog posts', () => {
-        const result = listHelper.totalLikes(blogs)
-        expect(result).toBe(36)
+    test('the function finds out which blog has most likes', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual({
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
     })
 })
--- part4/utils/listHelper.js
+++ part4/utils/listHelper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((acc, currentValue) =>
+        currentValue.likes > acc.likes ? currentValue : acc
+    )
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
