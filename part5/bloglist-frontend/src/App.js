a7cd3029299f7cde7fbaca10f9ffcdaa2dd8fb35
milon2000 <milakozlowska2000@gmail.com>
2022-02-01 15:01:49 +0100

    feat: add login form

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -5,14 +5,44 @@ import blogService from './services/blogs'
 const App = () => {
   const [blogs, setBlogs] = useState([])
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input 
+            type = "text"
+            value = {username}
+            name = "Username"
+            onchange = {({target}) => setUsername(target.value)}>
+            </input>  
+        </div>
+        <div>
+          password
+            <input
+            type = "text"
+            value = "password"
+            name = "Password"
+            onchange = {({target}) => setPassword(target.value)}
+            ></input>
+        </div> 
+        <button type = "submit">login</button>
+      </form>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
