a5e49e1734dc1a65900bcae0c39ba75f077b56a2
milon2000 <milakozlowska2000@gmail.com>
2022-02-03 15:19:12 +0100

    feat: add user token to local storage

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -25,6 +25,16 @@ const App = () => {
     )  
   }, [])
 
+  //app spr czy dane logowania sa w local storage
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -32,6 +42,11 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
