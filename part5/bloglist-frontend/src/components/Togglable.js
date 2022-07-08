23ce3f84febf005b02eaf8a0c13b3b0bc30d6e30
milon2000 <milakozlowska2000@gmail.com>
2022-07-08 14:40:27 +0200

    feat:PropTypes (5.11)

--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,11 +1,16 @@
 import React, { useState, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
 
 const Togglable = React.forwardRef((props, ref) => {
+
+
     const [visible, setVisible] = useState(false)
 
     const hideWhenVisible = { display: visible ?    'none' : ''}
     const showWhenVisible = { display: visible ?    '' : 'none'}
+
+  
 
     const toggleVisibility = () => {
         setVisible(!visible)
@@ -30,5 +35,8 @@ const Togglable = React.forwardRef((props, ref) => {
     )
 })
 
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
