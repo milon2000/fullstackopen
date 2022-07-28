a92b938949fb3ce6e263589b2ba36df06c150704
milon2000 <milakozlowska2000@gmail.com>
2022-07-28 09:40:49 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,20 +1,42 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('blog componenet', () => {
     const blog = {
         title: 'Title18',
-        author: 'Author18'
+        author: 'Author18',
+        url: 'www.18.com',
+        likes: 18
     }
+let component;
 
-    const { container } = render(<Blog blog={blog}/>)
+beforeEach(() => {
+    component = render(<Blog blog={blog}/>)
+})
+
+test('renders content', () => {
+
     //screen.debug()
-    const element = container.querySelector('.blog-title')
-    const element_author = container.querySelector('.blog-author')
+    const element = component.container.querySelector('.blog-title')
+    const element_author = component.container.querySelector('.blog-author')
     expect(element).toHaveTextContent('Title18')
     expect(element_author).toHaveTextContent('Author18')
 })
+
+test('url and number of likes on a click', () => {
+    const button = component.container.querySelector('button');
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent('www.18.com')
+    expect(component.container).toHaveTextContent('18')
+})
+    
+})
+
+
+
+
 
 
