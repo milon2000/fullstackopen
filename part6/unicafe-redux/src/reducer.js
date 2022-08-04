f9fc650dd182f756879ac3c2ecb5639eb536a230
milon2000 <milakozlowska2000@gmail.com>
2022-08-04 12:01:24 +0200

    unicafe with redux

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -4,20 +4,40 @@ const initialState = {
   bad: 0
 }
 
-const counterReducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      const newStateGood = {
+        good: state.good + 1,
+        ok: state.ok,
+        bad: state.bad
+      }
+      return newStateGood
     case 'OK':
-      return state
+      const newStateOk = {
+        good: state.good,
+        ok: state.ok + 1,
+        bad: state.bad
+      }
+      return newStateOk
     case 'BAD':
-      return state
+      const newStateBad = {
+        good: state.good,
+        ok: state.ok,
+        bad: state.bad +1
+      }
+      return newStateBad
     case 'ZERO':
-      return state
+      const newStateZero = {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
+      return newStateZero
     default: return state
   }
   
 }
 
-export default counterReducer
+export default reducer
