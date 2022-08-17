5f2b047e6f5a79fbacdf59d031f86a054ace6d5d
milon2000 <milakozlowska2000@gmail.com>
2022-08-17 08:15:20 +0200

    6.3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,11 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux'
+import finalPropsSelectorFactory from 'react-redux/es/connect/selectorFactory'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
+  const voteId = (id) => {
+    return {
+      type: 'VOTE',
+      data: {id}
+    }
+  }
+
   const vote = (id) => {
     console.log('vote', id)
+    console.log('anecdotes', anecdotes)
+    dispatch(voteId(id))
   }
 
   return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,20 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdotes = {
+        ...anecdoteToChange, 
+        votes: anecdoteToChange.votes + 1
+      }
+
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdotes)
+    default:
+      return state
+  }
+
 }
 
 export default reducer
