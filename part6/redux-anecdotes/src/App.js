fbcd1e3c4e3a9c312443f5353006ab4ffb9c8052
milon2000 <milakozlowska2000@gmail.com>
2022-11-30 15:55:08 +0100

    6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,36 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { vote } from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 
-const App = (props) => {
-  const anecdotes = useSelector(state => state)
-  const anecdotesSorted = [...anecdotes].sort((a, b) => {
-      return b.votes - a.votes
-    })
-
-
-  const dispatch = useDispatch()
-  const voteId = (id) => {
-    dispatch(vote(id))
-  }
-
+const App = () => {
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotesSorted.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => voteId(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-    <AnecdoteForm/>
+      <AnecdoteList/>
+      <AnecdoteForm/>
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { vote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+
+    const anecdotes = useSelector(state => state)
+    const anecdotesSorted = [...anecdotes].sort((a, b) => {
+        return b.votes - a.votes
+      })
+  
+  
+    const dispatch = useDispatch()
+    const voteId = (id) => {
+      dispatch(vote(id))
+    }
+
+    return (
+        <div>
+        {anecdotesSorted.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => voteId(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+        </div>
+    )
+}
+
+export default AnecdoteList
+
